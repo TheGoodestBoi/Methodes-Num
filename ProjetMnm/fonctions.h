a969413916e730a38e717ed8c1c3bdbed036eea1
The Goodest Boi <34127828+TheGoodestBoi@users.noreply.github.com>
2017-12-10 19:54:55 +0100

    Nouvelle disposition
    L'execution se fait dans le main 
    Tout le reste va dans le fichier fonctions

---
+++ ProjetMnm/fonctions.h
@@ -0,0 +1,3 @@
+void Cholesky(int dim, double mat[dim][dim] , double L[dim][dim] , double Lt[dim][dim]);
+void RempM (int dim, double Mat[dim][dim]);
+double puissance_inverse(int n, double eps, double A[n][n], double x[n], double x0[n]);
