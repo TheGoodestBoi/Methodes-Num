504ab64e117a0b0cac24ef859f77607224dededb
The Goodest Boi <34127828+TheGoodestBoi@users.noreply.github.com>
2017-12-21 13:50:32 +0100

    Delete fonctions.h

--- ProjetMnm/fonctions.h
+++
@@ -1,3 +0,0 @@
-void Cholesky(int dim, double mat[dim][dim] , double L[dim][dim] , double Lt[dim][dim]);
-void RempM (int dim, double Mat[dim][dim]);
-double puissance_inverse(int n, double eps, double A[n][n], double x[n], double x0[n]);
